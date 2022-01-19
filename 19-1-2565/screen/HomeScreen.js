29265ee98badea8bc33b4cdf28b99f8aafc009ea
Eskimoxd <95326736+Eskimoxd@users.noreply.github.com>
2022-01-19 11:15:06 +0700

    Add files via upload

---
+++ 19-1-2565/screen/HomeScreen.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {StyleSheet, Text, View, Button} from 'react-native';
+
+//import Ionicons from 'react-native-vector-icons/Ionicons';
+
+const HomeScreen = ({navigation}) => {
+  return (
+    <View style={styles.container}>
+      {/*<Ionicons name='brush' size={30} color='skyblue' />*/}
+      <Text>หน้าหลัก</Text>
+      <Button
+        title="Go to About"
+        onPress={() => navigation.navigate('About',
+        {email:'bo.peerapol_st@tni.ac.th'}
+        )}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default HomeScreen;
